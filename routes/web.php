9e6ce1d818584db89cd0bb07740823f075684e21
TuyulGanteng02 <147491049+TuyulGanteng02@users.noreply.github.com>
2024-01-08 20:28:32 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,19 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/home', 'HomeController@home')->name('home');
+Route::get('/login', function (){
+    return view('login');
+});
 
-Route::get('/login', 'LoginController@showLoginForm')->name('login');
+Route::get('/register', function (){
+    return view('register');
+});
+
+Route::get('/home', 'HomeController@home')->name('home');
+Route::post('/store-task', 'HomeController@store');
+
+Route::get('/login', 'LoginController@login')->name('login');
 Route::post('/login', 'LoginController@login');
 
-Route::get('/register', 'RegisterController@showRegistrationForm')->name('register');
+Route::get('/register', 'RegisterController@register')->name('register');
 Route::post('/register', 'RegisterController@register');
